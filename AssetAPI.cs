80d38ded0aa4eecbd53d528ee01445f8be571fec
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-10-21 10:16:14 -0300

    Add project files.

---
+++ AssetAPI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reptile;
+using UnityEngine;
+
+namespace CommonAPI
+{
+    public static class AssetAPI
+    {
+        private static Dictionary<ShaderNames, Shader> CachedShaders = new Dictionary<ShaderNames, Shader>();
+        public enum ShaderNames
+        {
+            AmbientCharacter,
+            AmbientEnvironment,
+            AmbientEnvironmentCutout,
+            AmbientEnvironmentTransparent
+        }
+        public static Shader GetShader(ShaderNames shaderName)
+        {
+            if (CachedShaders.TryGetValue(shaderName, out Shader result))
+                return result;
+            var assets = Core.Instance.Assets;
+            switch(shaderName)
+            {
+                case ShaderNames.AmbientCharacter:
+                    var shellMat = assets.LoadAssetFromBundle<Material>("common_assets", "shell");
+                    var shellShader = shellMat.shader;
+                    CacheShader(shaderName, shellShader);
+                    return shellShader;
+
+                case ShaderNames.AmbientEnvironment:
+                    var poleMat = assets.LoadAssetFromBundle<Material>("common_assets", "SkateboardScrewPoleMat");
+                    var poleShader = poleMat.shader;
+                    CacheShader(shaderName, poleShader);
+                    return poleShader;
+
+                case ShaderNames.AmbientEnvironmentCutout:
+                    var preludeMat = assets.LoadAssetFromBundle<Material>("common_assets", "Prelude_PropsAtlasMat");
+                    var preludeShader = preludeMat.shader;
+                    CacheShader(shaderName, preludeShader);
+                    return preludeShader;
+
+                case ShaderNames.AmbientEnvironmentTransparent:
+                    var discMat = assets.LoadAssetFromBundle<Material>("common_assets", "MusicCollectableMiniDiscTransperantMat");
+                    var discShader = discMat.shader;
+                    CacheShader(shaderName, discShader);
+                    return discShader;
+            }
+            throw new ArgumentOutOfRangeException("shaderName", "Shader name is out of range!");
+        }
+        private static void CacheShader(ShaderNames shaderName, Shader shader)
+        {
+            CachedShaders[shaderName] = shader;
+        }
+    }
+}
---
+++ CommonAPIPlugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using Reptile;
+using UnityEngine;
+
+namespace CommonAPI
+{
+    [BepInPlugin(GUID, Name, Version)]
+    internal class CommonAPIPlugin : BaseUnityPlugin
+    {
+        private const string GUID = "com.LazyDuchess.BRC.CommonAPI";
+        private const string Name = "CommonAPI";
+        private const string Version = "1.0.0";
+        private void Awake()
+        {
+            Logger.LogInfo($"{Name} {Version} loaded!");
+            StageManager.OnStagePostInitialization += StageManager_OnStagePostInitialization;
+        }
+
+        private void StageManager_OnStagePostInitialization()
+        {
+            Logger.LogInfo("Trying to load some shaders...");
+            Logger.LogInfo($"AmbientCharacter: {AssetAPI.GetShader(AssetAPI.ShaderNames.AmbientCharacter)}");
+            Logger.LogInfo($"AmbientEnvironment: {AssetAPI.GetShader(AssetAPI.ShaderNames.AmbientEnvironment)}");
+            Logger.LogInfo($"AmbientEnvironmentCutout: {AssetAPI.GetShader(AssetAPI.ShaderNames.AmbientEnvironmentCutout)}");
+            Logger.LogInfo($"AmbientEnvironmentTransparent: {AssetAPI.GetShader(AssetAPI.ShaderNames.AmbientEnvironmentTransparent)}");
+
+            var ambmat = new Material(AssetAPI.GetShader(AssetAPI.ShaderNames.AmbientEnvironment));
+            var playa = WorldHandler.instance.GetCurrentPlayer();
+            var comps = playa.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach(var comp in comps)
+            {
+                comp.sharedMaterial = ambmat;
+            }
+        }
+    }
+}
